419f8ece6b1f6c4ef357e3ba2deb2b328a0e2b42
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-03 22:54:15 +0100

    Refactor isOwner.php to use correct variable for user

--- isLogged/isOwner.php
+++ isLogged/isOwner.php
@@ -2,11 +2,11 @@
     session_start();
     require_once __DIR__ . "/../controllers/CRUDController.php";
 
-    $crud = new CRUDContoller("users", "*", "role", 1);
+    $user = new CRUDContoller("users", "*", "role", 1);
 
-    $getUser = $crud->conditionSelect();
+    $getUser = $user->conditionSelect();
 
-    if(!isset($_SESSION['user']) || $_SESSION['user'] != $getUser[0]['id']) {
+    if(!isset($_SESSION['user']) || $_SESSION['user']['id'] != $getUser['id']) {
         header('location:/resources/views/blog/blog.php');
     }
 ?>
