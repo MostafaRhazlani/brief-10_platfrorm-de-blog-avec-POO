06f9d4d6436b93027790385b2fd2b38427970ae7
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-03 20:36:40 +0100

    Hash password before user creation in newUser.php

--- resources/views/auth/newUser.php
+++ resources/views/auth/newUser.php
@@ -59,7 +59,8 @@
         }
     
         if(count($countErrors) == 0) {
-            $user = new UserController($username, $email, $password, $role, $imageProfile);
+            $password_hashed = password_hash($password, PASSWORD_BCRYPT);
+            $user = new UserController($username, $email, $password_hashed, $role, $imageProfile);
     
             if($user->createUser()) {
                 header('location: login.php');
