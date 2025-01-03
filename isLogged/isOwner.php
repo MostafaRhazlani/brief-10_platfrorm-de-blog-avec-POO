e6bac93cee2e7979ce0b177001b5cab97ff42b36
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-03 21:59:45 +0100

    Refactor isOwner.php to improve user validation and session checks

--- isLogged/isOwner.php
+++ isLogged/isOwner.php
@@ -2,10 +2,11 @@
     session_start();
     require_once __DIR__ . "/../controllers/CRUDController.php";
 
-    $crud = new CRUDContoller("users", "id", "role", 0);
+    $crud = new CRUDContoller("users", "*", "role", 1);
 
     $getUser = $crud->conditionSelect();
-    if(!isset($_SESSION['user']) || $_SESSION['user']['id'] != $getUser['id']) {
+
+    if(!isset($_SESSION['user']) || $_SESSION['user'] != $getUser[0]['id']) {
         header('location:/resources/views/blog/blog.php');
     }
 ?>
