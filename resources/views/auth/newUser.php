a8217113508d14d13dd99302fa7472a89fc0bb1a
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-03 14:52:38 +0100

    Add UserController for user creation and refactor newUser.php

--- resources/views/auth/newUser.php
+++ resources/views/auth/newUser.php
@@ -1,75 +1,74 @@
 <?php
     session_start();
-    require_once('../../../connectdb/connectiondb.php');
+    require __DIR__ . '/../../../controllers/UserController.php';
 
-    $username = isset($_POST['username']) ? $_POST['username'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
-    $confirm_password = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : '';
-    $imageProfile = isset($_FILES['imageProfile']['name']) ? $_FILES['imageProfile']['name'] : '';
-    $tempImageProfile = isset($_FILES['imageProfile']['tmp_name']) ? $_FILES['imageProfile']['tmp_name'] : '';
-    $folder = '../../img/images/'. $imageProfile;
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $countErrors = array();
-
-
-    if(!empty($tempImageProfile)) {
-        move_uploaded_file($tempImageProfile, $folder);
-    } else {
-        $imageProfile = 'default.png';
-    }
-
-    // check if input username is empty
-    if(empty($username)) {
-        $countErrors[] = 1;
-        $_SESSION['emptyUsername'] = 'username field is required!';
-
-    }
-
-    // check if input email is empty
-    if(empty($email)) {
-        $countErrors[] = 1;
-        $_SESSION['emptyEmail'] = 'email field is required!';
-    }
-
-    // check if form email is valid
-    if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $countErrors[] = 1;
-        $_SESSION['invalidEmail'] = 'form of email is invalid!';
-    }
-
-    // check if input password is empty
-    if(empty($password)) {
-        $countErrors[] = 1;
-        $_SESSION['emptyPassword'] = 'password field is required!';
-    }
-
-    // check if user confirm password is empty
-    if(empty($confirm_password)) {
-        $countErrors[] = 1;
-        $_SESSION['emptyConfirmPassword'] = 'confirm password field is required!';
-    }
-    
-    // check if password and confirm password is same
-    if($password != $confirm_password) {
-        $countErrors[] = 1;
-        $_SESSION['notSamePassword'] = 'password is not correct!';
-    }
-
-    if(count($countErrors) == 0) {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        $role = 0;
+        $imageProfile = $_FILES['imageProfile']['name'];
+        $tempImageProfile = $_FILES['imageProfile']['tmp_name'];
+        $folder = '../../img/images/'. $imageProfile;
+        $countErrors = array();
         
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-        $idRole = 2;
-        $resultQuery = mysqli_prepare($conn, "INSERT INTO users(username, email, password, imageProfile, idRole) VALUES(?,?,?,?,?)");
-
-        mysqli_stmt_bind_param($resultQuery, "ssssi", $username, $email, $hashedPassword, $imageProfile, $idRole);
-
-        if(mysqli_stmt_execute($resultQuery)) {
-            header('location: login.php');
+    
+        if(!empty($tempImageProfile)) {
+            move_uploaded_file($tempImageProfile, $folder);
         } else {
-            echo "error";
+            $imageProfile = 'default.png';
         }
-    } else {
-        header('location:register.php');
+    
+        // check if input username is empty
+        if(empty($username)) {
+            $countErrors[] = 1;
+            $_SESSION['emptyUsername'] = 'username field is required!';
+    
+        }
+    
+        // check if input email is empty
+        if(empty($email)) {
+            $countErrors[] = 1;
+            $_SESSION['emptyEmail'] = 'email field is required!';
+        }
+    
+        // check if form email is valid
+        if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $countErrors[] = 1;
+            $_SESSION['invalidEmail'] = 'form of email is invalid!';
+        }
+    
+        // check if input password is empty
+        if(empty($password)) {
+            $countErrors[] = 1;
+            $_SESSION['emptyPassword'] = 'password field is required!';
+        }
+    
+        // check if user confirm password is empty
+        if(empty($confirm_password)) {
+            $countErrors[] = 1;
+            $_SESSION['emptyConfirmPassword'] = 'confirm password field is required!';
+        }
+        
+        // check if password and confirm password is same
+        if($password != $confirm_password) {
+            $countErrors[] = 1;
+            $_SESSION['notSamePassword'] = 'password is not correct!';
+        }
+    
+        if(count($countErrors) == 0) {
+            $user = new UserController($username, $email, $password, $role, $imageProfile);
+    
+            if($user->createUser()) {
+                header('location: login.php');
+            } else {
+                echo "not registred";
+            }
+        } else {
+            header('location:register.php');
+        }
     }
+
 ?>
