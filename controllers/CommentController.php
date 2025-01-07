c4276d975e6940c4ae5e09ad2241d405f8d048b3
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-07 19:30:17 +0100

    add CommentController to fetch comments for a specific article

---
+++ controllers/CommentController.php
@@ -0,0 +1,22 @@
+<?php
+    require_once __DIR__ . '/CRUDController.php';
+
+    class CommentController {
+        private $idArticle;
+
+        public function __construct($idArticle){
+            $this->idArticle = $idArticle;
+        }
+
+        public function index() {
+            $db = new DB();
+            $conn = $db->connect();
+
+            $result = $conn->query("SELECT comments.*, email, username, imageProfile, role FROM comments JOIN users ON users.id = comments.idUser WHERE idArticle = $this->idArticle ORDER BY id DESC");
+
+            return $result->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+
+?>
