f2ad38008e877eb94352dead7764e9f1695f4f22
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-01-11 23:01:03 +0100

    add right sidebar for suggested friends in layout

---
+++ resources/views/layout/__RIGHTBAR.php
@@ -0,0 +1,49 @@
+<div class="hidden lg:flex flex-col w-1/4 text-white">
+    <div class="h-screen pt-24">
+        <div class="h-full pb-2">
+            <div class="bg-[#202225] rounded-lg p-4 h-full">
+                <h1>suggested friends</h1>
+                <div class="flex items-center justify-between mt-5">
+                    <div class="flex">
+                        <img class="w-12 h-12 rounded-full mr-2" src="../../img/images/photo youcode.jpg" alt="">
+                        <div >
+                            <h2 class="-mb-1">Othmane Rhazlani</h2>
+                            <span class="text-sm text-gray-400">@othmane</span>
+                        </div>
+                    </div>
+                    <span class="px-3 py-1 border bg-opacity-10 text-sm bg-[#FFD37D] border-[#FFD37D] text-[#FFD37D] rounded-full">Follow</span>
+                </div>
+                <div class="flex items-center justify-between mt-5">
+                    <div class="flex">
+                        <img class="w-12 h-12 rounded-full mr-2" src="../../img/images/photo youcode.jpg" alt="">
+                        <div >
+                            <h2 class="-mb-1">Othmane Rhazlani</h2>
+                            <span class="text-sm text-gray-400">@othmane</span>
+                        </div>
+                    </div>
+                    <span class="px-3 py-1 border bg-opacity-10 text-sm bg-[#FFD37D] border-[#FFD37D] text-[#FFD37D] rounded-full">Follow</span>
+                </div>
+                <div class="flex items-center justify-between mt-5">
+                    <div class="flex">
+                        <img class="w-12 h-12 rounded-full mr-2" src="../../img/images/photo youcode.jpg" alt="">
+                        <div >
+                            <h2 class="-mb-1">Othmane Rhazlani</h2>
+                            <span class="text-sm text-gray-400">@othmane</span>
+                        </div>
+                    </div>
+                    <span class="px-3 py-1 border bg-opacity-10 text-sm bg-[#FFD37D] border-[#FFD37D] text-[#FFD37D] rounded-full">Follow</span>
+                </div>
+                <div class="flex items-center justify-between mt-5">
+                    <div class="flex">
+                        <img class="w-12 h-12 rounded-full mr-2" src="../../img/images/photo youcode.jpg" alt="">
+                        <div >
+                            <h2 class="-mb-1">Othmane Rhazlani</h2>
+                            <span class="text-sm text-gray-400">@othmane</span>
+                        </div>
+                    </div>
+                    <span class="px-3 py-1 border bg-opacity-10 text-sm bg-[#FFD37D] border-[#FFD37D] text-[#FFD37D] rounded-full">Follow</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
